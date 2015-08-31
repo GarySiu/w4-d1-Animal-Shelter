e2e2f886901300fb009ffcc868d859b0bd94604f
Gary Siu <Gary@untypeable.com>
2015-08-31 19:21:49 +0100

    Added an additional class for the shelter and finished the currently non-functional menu

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require_relative 'animals'
 require_relative 'clients'
+require_relative 'shelters'
 
+shelter = Shelter.new('Animal Hospital')
 
 ## TEST VALUES ##
 # client = Client.new('Bob','m')
@@ -8,3 +10,21 @@ require_relative 'clients'
 
 # animal = Animal.new('Spot','Dalmatian')
 # puts "There is a pet named #{animal.name} and they are are #{animal.breed}"
+
+def menu
+  puts `clear`
+  puts '*' * 52
+  puts '* Welcome to Animal Hospital *'.center 50
+  puts '*' * 52
+  puts '1: Register a new client'
+  puts '2: List registered clients'
+  puts '3: Register a new animal'
+  puts '4: List registered animals'
+  puts '5: Give up animal for adoption'
+  puts '6: List animals availible for adoption'
+  puts 'q: Quit'
+  print '--> '
+  gets.chomp
+end
+
+response = menu
---
+++ shelters.rb
@@ -0,0 +1,6 @@
+class Shelter
+  def initialize(name)
+    @name = name
+  end
+  attr_reader :name
+end
