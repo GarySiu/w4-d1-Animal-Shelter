1c7742f67406c0daa734a5303d33ef1f1ba397ed
Gary Siu <Gary@untypeable.com>
2015-08-31 18:25:29 +0100

    initial commit. Some basic class def for clients

---
+++ clients.rb
@@ -0,0 +1,9 @@
+class Clients
+  def initialize('name','gender','age')
+    @name = name
+    @gender = gender
+    @age = age
+    @number_of_pets = ''
+
+  end
+end
---
+++ main.rb
@@ -0,0 +1,2 @@
+require_relative 'animals'
+require_relative 'clients'
